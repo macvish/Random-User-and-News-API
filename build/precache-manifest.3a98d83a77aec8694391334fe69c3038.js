c4b862b89aec85353571a77d11360f6320f08a92
Macanthony Ahaotu <macintony1@gmail.com>
2019-11-15 08:30:01 +0100

    Build

---
+++ build/precache-manifest.3a98d83a77aec8694391334fe69c3038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8972217bedfb961880231a69b9ad2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a458d8f7ae42574dae",
+    "url": "/static/css/2.5d4e301c.chunk.css"
+  },
+  {
+    "revision": "105e0899aa713d81d336",
+    "url": "/static/css/main.83ebb46f.chunk.css"
+  },
+  {
+    "revision": "a6a458d8f7ae42574dae",
+    "url": "/static/js/2.08d9e6e1.chunk.js"
+  },
+  {
+    "revision": "105e0899aa713d81d336",
+    "url": "/static/js/main.00a59586.chunk.js"
+  },
+  {
+    "revision": "0e29ddd92ddbe64c311a",
+    "url": "/static/js/runtime-main.9353beb3.js"
+  }
+]);
